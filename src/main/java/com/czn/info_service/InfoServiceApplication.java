369b1fba3e9b2e0ee0fa1591d0b3c6eed68a148a
suhrud-caizin <suhrud@caizin.com>
2024-09-30 16:23:35 +0530

    first

---
+++ src/main/java/com/czn/info_service/InfoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.czn.info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class InfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoServiceApplication.class, args);
+	}
+
+}
